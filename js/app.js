8aec641e9ef0d2b6202f7d957bd7c8607f4fb93e
Eddie Kang <buildafireapp@gmail.com>
2018-11-19 18:05:41 -0800

    styling classes

--- js/app.js
+++ js/app.js
@@ -5,19 +5,27 @@ function loadTasks(taskList){
 
     taskList.forEach((task,i)=>{
         let divTask = document.createElement('div');
-        divTask.className="taskCard";
+        divTask.className="taskCard list-group-item";
         divTask.innerHTML = `
-            ${task.img?"<img src='" + task.img + "' />":""}
-            <h4> <input type="checkbox" ${task.completed?"checked":""} title="check completed">  ${task.title + ' ' + i}</h4>
-            <span>created on ${task.createdOn} by ${task.createdBy}</span>
-            <p>${task.description}</p>
-            <span>Due on ${task.dueDate}</span>            
+            ${task.img?"<img class='img-fluid' src='" + task.img + "' />":""}
+            <h4>
+            <div class="input-group mb-3">
+            <div class="input-group-prepend">
+            <div class="input-group-text">
+                <input type="checkbox" ${task.completed?"checked":""} title="check completed"> 
+            </div></div> 
+            <input type="text" class="form-control" placeholder="${task.title + ' ' + i}">
+            </div>
+            </h4>
+            <span class="small">created on ${task.createdOn} by ${task.createdBy}</span>
+            <p class="text-justify">${task.description}</p>
+            <span class="due shadow p-1 mb-5 bg-white rounded">Due on ${task.dueDate}</span>            
         `;
 
         if(task.completed)
-            divTask.classList.add("taskCompleted");
+            divTask.classList.add("list-group-item-success");
         else if(task.dueDate < Date.now() )
-            divTask.classList.add("taskLate");
+            divTask.classList.add("list-group-item-danger");
 
 
         container.appendChild(divTask);
